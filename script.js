74131b227b994ec4209642dde2ba827603d9f9fc
Stefano <stefanopinna92@hotmail.it>
2023-03-10 14:48:20 +0000

    Working on calculator style

---
+++ script.js
@@ -0,0 +1,19 @@
+let add = function(a, b) {
+    return a + b; 
+};
+
+let subtract = function(a, b) {
+    return a - b; 
+};
+
+let multiply = function(a, b) {
+    return a * b; 
+};
+
+let divide = function(a, b) {
+    return a / b; 
+};
+
+let operate = function(a, b, c) {
+    return a(b, c);
+}

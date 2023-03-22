7f4ad0e04914bc639175f838f4e0a8313bffdb66
Stefano <stefanopinna92@hotmail.it>
2023-03-22 15:06:33 +0000

    Events linked to buttons

--- script.js
+++ script.js
@@ -17,3 +17,20 @@ let divide = function(a, b) {
 let operate = function(a, b, c) {
     return a(b, c);
 }
+
+let display = document.querySelector('#result-bar');
+
+
+let btn = document.querySelectorAll('button');
+
+console.log(btn);
+btn.forEach(function(i) {
+     i.addEventListener('click', function(e) {
+
+        alert(e.target.innerHTML);
+         /* let digit = document.createElement('p');
+         digit.textContent = e.target.textContent;
+         display.appendChild(digit);  */
+     })
+});
+
